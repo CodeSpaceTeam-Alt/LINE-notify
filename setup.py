6555e6e82df315adf43c0db92b8e46fca25c8330
Alt <iwano.toru.work@gmail.com>
2021-12-31 13:26:54 +0900

    update : metadata move setup.py -> setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 """, module
 setup module
 """
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="line-notify",
-    version="1.0.0",
-    install_requires=["requests"],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    entry_points={
-        "console_scripts": [
-            "line_notify = line_notify.utils:main"
-        ]
-    }
-)
+setup()
